192fe7f0472524e58c65e98e5f76c4b07e45e504
debbairagi <debbindubd@gmail.com>
2023-01-16 14:14:46 +0600

    Final Homework

--- cpp/sem22/binary_search.cpp
+++ cpp/sem22/binary_search.cpp
@@ -7,9 +7,10 @@ using namespace std;
 class bindu
 {
 public:
-    int arr[100], item, mid, min, max, i, n;
+    int data[100], item, mid, min, max, i, n, k, ptr;
 
     void input();
+    void sorting();
     void search();
 };
 
@@ -20,10 +21,25 @@ void bindu ::input()
     cout << "Enter the sorted array item: ";
     for (i = 1; i <= n; i++)
     {
-        cin >> arr[i];
+        cin >> data[i];
     }
     cout << "Enter the search item: ";
     cin >> item;
+}
+void bindu::sorting()
+{
+    for (k = 1; k <= n - 1; k++)
+    {
+        for (ptr = 1; ptr <= n - k; ptr++)
+        {
+            if (data[ptr] > data[ptr + 1])
+            {
+                int temp = data[ptr];
+                data[ptr] = data[ptr + 1];
+                data[ptr + 1] = temp;
+            }
+        }
+    }
 }
 
 void bindu ::search()
@@ -32,14 +48,13 @@ void bindu ::search()
     max = n;
     while (min <= max)
     {
-
         mid = (min + max) / 2;
-        if (arr[mid] == item)
+        if (data[mid] == item)
         {
             cout << "item found at location: " << mid;
             break;
         }
-        if (arr[mid] > item)
+        if (data[mid] > item)
         {
             max = mid - 1;
         }
@@ -57,6 +72,7 @@ int main()
 {
     bindu obj;
     obj.input();
+    obj.sorting();
     obj.search();
 
     return 0;
