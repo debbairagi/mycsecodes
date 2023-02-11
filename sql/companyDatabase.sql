011e0ed30fd71cad99af488158ec745e5d823b8b
debbairagi <debbindubd@gmail.com>
2023-02-11 13:28:41 +0600

    11-02

--- sql/companyDatabase.sql
+++ sql/companyDatabase.sql
@@ -234,3 +234,90 @@ WHERE client_name LIKE '%LLC';
 
 SELECT * FROM branch_supplier
 WHERE supplier_name LIKE '% Level%';
+
+
+--% = any # characters, _ = one character
+SELECT * 
+FROM employee
+WHERE birth_day LIKE '____-10%';
+
+
+-- Find any client who are schools
+
+SELECT * FROM client
+WHERE client_name LIKE '%School%';
+
+
+-- Using Union Query
+-- Find a list of employee and branch names
+SELECT first_name FROM employee
+UNION
+SELECT branch_name FROM branch;
+
+-- Using Union Query
+-- Find a list of employee and branch names
+SELECT first_name FROM employee
+UNION
+SELECT branch_name FROM branch
+UNION
+SELECT client_name FROM client;
+
+-- Find a list of all clients & branch supplier's names
+SELECT client_name FROM client
+UNION
+SELECT supplier_name FROM branch_supplier;
+
+-- Find a list of all clients & branch supplier's names
+SELECT client_name , client.branch_id
+FROM client
+UNION
+SELECT supplier_name, branch_supplier.branch_id
+FROM branch_supplier;
+
+-- Find a list of all money spent or earned by the company
+SELECT salary
+FROM employee
+UNION
+SELECT total_sales
+FROM works_with;
+
+-- Find all branches and the names of their managers
+SELECT employee.emp_id, employee.first_name, branch.branch_name
+FROM employee
+JOIN branch
+ON employee.emp_id = branch.mgr_id;
+
+-- Find all branches and the names of their managers
+SELECT employee.emp_id, employee.first_name, branch.branch_name
+FROM employee
+LEFT JOIN branch
+ON employee.emp_id = branch.mgr_id;
+
+-- Find all branches and the names of their managers
+SELECT employee.emp_id, employee.first_name, branch.branch_name
+FROM employee
+RIGHT JOIN branch
+ON employee.emp_id = branch.mgr_id;
+
+-- Nested Query in sql
+-- Find names of all employees who have 
+-- sold over 30,00 to a single client
+SELECT employee.first_name, employee.last_name
+FROM employee
+WHERE employee.emp_id IN (
+    SELECT works_with.emp_id
+	FROM works_with
+	WHERE works_with.total_sales > 30000
+);
+
+
+-- Find all clients who are handled by the branch
+-- that Micheal Scott manages
+-- Assume you know  Micheal's ID
+
+SELECT client.client_name
+FROM client
+WHERE client.branch_id = (
+    SELECT branch.branch_id FROM branch
+	WHERE branch.mgr_id = 102
+);
